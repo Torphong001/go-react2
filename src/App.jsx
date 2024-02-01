874395175cce280fb05ccf109fe05b772da3d661
Torphong001 <641463035@crru.ac.th>
2024-02-01 16:15:05 +0700

    1-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import ItemsList from './components/ItemsList';
-import ItemFormFind from './components/ItemFormFind';
-import SignIn from './components/SignIn';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ItemsList from "./components/ItemsList";
+import ItemFormFind from "./components/ItemFormFind";
+import SignIn from "./components/SignIn";
+import "./App.css";
+import Register from "./components/Register";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,33 +19,44 @@ function App() {
       <div>
         <h3>My Project</h3>
         <nav>
-            <Link to="/">หน้าหลัก</Link>
-            <hr />
-            {!isLoggedIn && (
-                <Link to="/signin">เข้าสู่ระบบ</Link>
-            )}
+          <Link to="/">หน้าหลัก</Link>
+          <hr />
+          {!isLoggedIn && <Link to="/signin">เข้าสู่ระบบ</Link>}
+          <hr />
+          {!isLoggedIn && <Link to="/register">สมัคร</Link>}
         </nav>
 
         {/* Add the Routes component to the App */}
         {/* <> xxx </>คือตำแหน่งที่ข้อมูลจาก Component คือค่ามา */}
         <Routes>
-          <Route path="/signin" element={<SignIn onLoginSuccess={handleLoginSuccess} />} />
-          <Route path="/" element={
-            <> 
-              <div className="card">
-                {isLoggedIn ? (
-                  <>
-                    <p>ยินดีต้อนรับ</p>
-                    <p><ItemsList/></p>
-                    <hr/>
-                    <p><ItemFormFind/></p>
-                  </>
-                ) : (
-                  <p>กรุณา Login </p>
-                )}
-              </div>
-            </>
-          }/>
+          <Route
+            path="/signin"
+            element={<SignIn onLoginSuccess={handleLoginSuccess} />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="card">
+                  {isLoggedIn ? (
+                    <>
+                      <p>ยินดีต้อนรับ</p>
+                      <p>
+                        <ItemsList />
+                      </p>
+                      <hr />
+                      <p>
+                        <ItemFormFind />
+                      </p>
+                    </>
+                  ) : (
+                    <p>กรุณา Login </p>
+                  )}
+                </div>
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
