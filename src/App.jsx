8f3a33cb3fa3d354a3731873ef6aad466c931893
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2024-02-01 12:41:53 +0700

    update remove unused lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ItemsList from './components/ItemsList';
 import ItemFormFind from './components/ItemFormFind';
